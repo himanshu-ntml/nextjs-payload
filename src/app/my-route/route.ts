2123fa984cf01bb9dd32d4d48521f171d753b3be
himanshu <himanshu@nicolethemathlady.com>
2024-03-27 21:56:45 +0530

    initial

---
+++ src/app/my-route/route.ts
@@ -0,0 +1,16 @@
+// export const runtime = 'edge'
+
+import { getPayload } from 'payload'
+import configPromise from '@payload-config'
+
+export const GET = async () => {
+  const payload = await getPayload({
+    config: configPromise,
+  })
+
+  const data = await payload.find({
+    collection: 'users',
+  })
+
+  return Response.json(data)
+}
